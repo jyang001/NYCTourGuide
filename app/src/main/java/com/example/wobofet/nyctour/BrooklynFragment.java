7a87682eea63ea2acf3726625ad28e98df6f98f8
jyang001 <joeyang095@gmail.com>
2018-04-24 19:56:20 -0400

    add basic Fragment files

--- app/src/main/java/com/example/wobofet/nyctour/BrooklynFragment.java
+++ app/src/main/java/com/example/wobofet/nyctour/BrooklynFragment.java
@@ -1,7 +1,5 @@
 package com.example.wobofet.nyctour;
-
 import android.support.v4.app.Fragment;
 
-
-public class ManhattanFragment extends Fragment {
+public class BrooklynFragment extends Fragment {
 }
--- app/src/main/java/com/example/wobofet/nyctour/QueensFragment.java
+++ app/src/main/java/com/example/wobofet/nyctour/QueensFragment.java
@@ -2,6 +2,5 @@ package com.example.wobofet.nyctour;
 
 import android.support.v4.app.Fragment;
 
-
-public class ManhattanFragment extends Fragment {
+public class QueensFragment extends Fragment {
 }

0e583dac910f0d37d3e4ea419eed8282cb76c771
jyang001 <joeyang095@gmail.com>
2018-04-26 23:37:10 -0400

    modify Adapter and Fragment files

--- app/src/main/java/com/example/wobofet/nyctour/AttractionAdapter.java
+++ app/src/main/java/com/example/wobofet/nyctour/AttractionAdapter.java
@@ -1,29 +1,20 @@
 package com.example.wobofet.nyctour;
 
-import android.app.Activity;
 import android.content.Context;
-import android.app.Activity;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.ContextCompat;
-import android.text.Layout;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
 import java.util.ArrayList;
 
 public class AttractionAdapter extends FragmentPagerAdapter {
 
-    public AttractionAdapter(FragmentManager fm) {
+    private Context aContext;
+
+    public AttractionAdapter(Context context, FragmentManager fm) {
         super(fm);
+        aContext = context;
     }
 
     @Override

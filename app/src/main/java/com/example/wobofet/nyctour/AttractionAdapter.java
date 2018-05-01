af67c8a6d4834ee67792875f1070de3fb807cb3f
jyang001 <joeyang095@gmail.com>
2018-04-30 23:31:56 -0400

    modify Attraction Adapter and strings

--- app/src/main/java/com/example/wobofet/nyctour/AttractionAdapter.java
+++ app/src/main/java/com/example/wobofet/nyctour/AttractionAdapter.java
@@ -1,6 +1,7 @@
 package com.example.wobofet.nyctour;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -37,6 +38,25 @@ public class AttractionAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 5;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return aContext.getString(R.string.category_brooklyn);
+        }
+        else if (position == 1) {
+            return aContext.getString(R.string.category_queens);
+        }
+        else if (position == 2) {
+            return aContext.getString(R.string.category_manhattan);
+        }
+        else if (position == 3) {
+            return aContext.getString(R.string.category_bronx);
+        }
+        else
+            return aContext.getString(R.string.category_staten_island);
     }
 
 }

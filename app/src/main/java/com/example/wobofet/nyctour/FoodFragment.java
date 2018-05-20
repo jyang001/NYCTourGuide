4b680755eecc4a89f21b2c278769e13b956fba13
jyang001 <joeyang095@gmail.com>
2018-05-20 02:27:05 -0400

    add fragments of categories

---
+++ app/src/main/java/com/example/wobofet/nyctour/FoodFragment.java
@@ -0,0 +1,15 @@
+package com.example.wobofet.nyctour;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FoodFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_food, container, false);
+    }
+}
